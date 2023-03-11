2b3b9da4164b55368cda23711a7cb25ab77be6a3
shivambindal93 <shivam.bindal@nagarro.com>
2023-03-11 20:38:37 +0530

    added firebase change

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
-
+const routes = require("./routes");
+const authMiddleware = require("./auth");
 let port = process.env.PORT || 3000;
 
-app.use('/api/v1', routes);
-app.listen(port, ()=>{
-  console.log(`The server is listening on port ${port}`)
-})
+app.use("/api/v1", routes);
+app.use("/", authMiddleware);
+app.listen(port, () => {
+  console.log(`The server is listening on port ${port}`);
+});
