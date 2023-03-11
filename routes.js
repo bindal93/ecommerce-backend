719807003cf0aaeafc0b4d0b4cef2dc4ae8ad838
shivambindal93 <shivam.bindal@nagarro.com>
2023-03-11 20:40:47 +0530

    prettier and git-ignore change

--- routes.js
+++ routes.js
@@ -1,127 +1,133 @@
-const express = require('express');
-const router =  express.Router();
-const elastic = require('elasticsearch');
-const bodyParser = require('body-parser').json();
+const express = require("express");
+const router = express.Router();
+const elastic = require("elasticsearch");
+const bodyParser = require("body-parser").json();
 const elasticClient = elastic.Client({
-  host: 'localhost:9200',
+  host: "localhost:9200"
 });
 
-router.use((req, res, next)=>{
-  elasticClient.index({
-    index: 'logs',
-    body: {
-      url: req.url,
-      method: req.method,
-    }
-  })
-  .then(res=>{
-    console.log('Logs indexed')
-  })
-  .catch(err=>{
-    console.log(err)
-  })
+router.use((req, res, next) => {
+  elasticClient
+    .index({
+      index: "logs",
+      body: {
+        url: req.url,
+        method: req.method
+      }
+    })
+    .then((res) => {
+      console.log("Logs indexed");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   next();
 });
 
-router.post('/products', bodyParser, (req, res)=>{
-  elasticClient.index({
-    index: 'products',
-    body: req.body
-  })
-  .then(resp=>{
-    return res.status(200).json({
-      msg: 'product indexed'
+router.post("/products", bodyParser, (req, res) => {
+  elasticClient
+    .index({
+      index: "products",
+      body: req.body
+    })
+    .then((resp) => {
+      return res.status(200).json({
+        msg: "product indexed"
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        msg: "Error",
+        err
+      });
     });
-  })
-  .catch(err=>{
-    return res.status(500).json({
-      msg: 'Error',
-      err
-    });
-  })
 });
 
-router.get('/products/:id', (req, res)=>{
+router.get("/products/:id", (req, res) => {
   let query = {
-    index: 'products',
+    index: "products",
     id: req.params.id
-  }
-  elasticClient.get(query)
-  .then(resp=>{
-    if(!resp){
-      return res.status(404).json({
+  };
+  elasticClient
+    .get(query)
+    .then((resp) => {
+      if (!resp) {
+        return res.status(404).json({
+          product: resp
+        });
+      }
+      return res.status(200).json({
         product: resp
       });
-    }
-    return res.status(200).json({
-      product: resp
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        msg: "Error not found",
+        err
+      });
     });
-  })
-  .catch(err=>{
-    return res.status(500).json({
-      msg: 'Error not found',
-      err
-    });
-  });
 });
 
-router.put('/products/:id', bodyParser, (req, res)=>{
-  elasticClient.update({
-    index: 'products',
-    id: req.params.id,
-    body: {
-      doc: req.body
-    }
-  })
-  .then(resp=>{
-    return res.status(200).json({
-      msg: 'product updated'
+router.put("/products/:id", bodyParser, (req, res) => {
+  elasticClient
+    .update({
+      index: "products",
+      id: req.params.id,
+      body: {
+        doc: req.body
+      }
+    })
+    .then((resp) => {
+      return res.status(200).json({
+        msg: "product updated"
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        msg: "Error",
+        err
+      });
     });
-  })
-  .catch(err=>{
-    console.log(err)
-    return res.status(500).json({
-      msg: 'Error',
-      err
-    });
-  })
 });
 
-router.delete('/products/:id', (req, res)=>{
-  elasticClient.delete({
-    index: 'products',
-    id: req.params.id
-  })
-  .then(resp=>{
-    res.status(200).json({
-      'msg': 'Product deleted'
+router.delete("/products/:id", (req, res) => {
+  elasticClient
+    .delete({
+      index: "products",
+      id: req.params.id
+    })
+    .then((resp) => {
+      res.status(200).json({
+        msg: "Product deleted"
+      });
+    })
+    .catch((err) => {
+      res.status(404).json({
+        msg: "Error"
+      });
     });
-  })
-  .catch(err=>{
-    res.status(404).json({
-      'msg': 'Error'
-    });
-  });
 });
 
-router.get('/products', (req, res)=>{
+router.get("/products", (req, res) => {
   let query = {
-    index: 'products'
-  }
-  if (req.query.product) query.q =  `*${req.query.product}*`;
-  elasticClient.search(query)
-  .then(resp=>{
-    return res.status(200).json({
-      products: resp.hits.hits
+    index: "products"
+  };
+  if (req.query.product) query.q = `*${req.query.product}*`;
+  elasticClient
+    .search(query)
+    .then((resp) => {
+      return res.status(200).json({
+        products: resp.hits.hits
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        msg: "Error",
+        err
+      });
     });
-  })
-  .catch(err=>{
-    console.log(err);
-    return res.status(500).json({
-      msg: 'Error',
-      err
-    });
-  });
 });
 
 module.exports = router;
