a3f3e85a5fe8fc82329985690bea5b97b3054970
shivambindal93 <shivam.bindal@nagarro.com>
2023-03-17 20:42:34 +0530

    updated code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 const authorization = require("./authorization");
 let port = process.env.PORT || 5000;
 app.use(cors());
-app.use("/", authorization);
+// app.use("/", authorization);
 app.use("/api/v1", cors(), routes);
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}`);
