3f18c8027b2d91f469d5ca748614955584a83da2
shivambindal93 <shivam.bindal@nagarro.com>
2023-03-26 17:31:24 +0530

    updated code

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 const authorization = require("./authorization");
 let port = process.env.PORT || 5000;
 app.use(cors());
+app.use(
+  helmet({
+    referrerPolicy: { policy: "no-referrer" }
+  })
+);
 // app.use("/", authorization);
 app.use("/api/v1", cors(), routes);
 const server = app.listen(port, () => {
