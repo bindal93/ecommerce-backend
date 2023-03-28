2495d167d65a982d8b751eef35f0bf51c7d262da
shivambindal93 <shivam.bindal@nagarro.com>
2023-03-28 22:16:30 +0530

    updated code changes

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
-const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 const authorization = require("./authorization");
 let port = process.env.PORT || 5000;
 app.use(cors());
-// app.use(
-//   helmet({
-//     referrerPolicy: { policy: "no-referrer" },
-//   })
-// );
 // app.use("/", authorization);
-app.use("/", (request, response, next) => {
-  return response.status(200).send({ message: "response generated" });
-});
 app.use("/api/v1", cors(), routes);
 const server = app.listen(port, () => {
   console.log(`The server is listening on port ${port}`);
 });
-
+app.use((request, response, next) => {
+  if (request.path === "/") {
+    response.status(200).send({ message: "response generated" });
+  } else {
+    next();
+  }
+});
 module.exports = { app, server };
